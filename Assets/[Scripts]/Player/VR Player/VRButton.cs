59b2a793c16603992067e5b5776ed90a47a93fd0
TobxtR <hantian.chew@gmail.com>
2024-04-09 22:08:37 +0800

    button can no longer be pushed x and z direction

--- Assets/[Scripts]/Player/VR Player/VRButton.cs
+++ Assets/[Scripts]/Player/VR Player/VRButton.cs
@@ -14,7 +14,7 @@ public class VRButton : MonoBehaviour
     private bool isPressed = false;
     private Vector3 startPos;
     private ConfigurableJoint joint;
-
+    Vector3 startingPosition;
     private void Start()
     {
         startPos = button.transform.localPosition;
@@ -31,12 +31,14 @@ public class VRButton : MonoBehaviour
         {
             OnRelease();
         }
-
+        //make sure button's x and z never move
+        button.transform.localPosition = new Vector3(startPos.x, button.transform.localPosition.y, startPos.z);
         // Prevent the button from moving "upwards" beyond its starting position
         if (button.transform.localPosition.y > startPos.y)
         {
             button.transform.localPosition = new Vector3(button.transform.localPosition.x, startPos.y, button.transform.localPosition.z);
         }
+
     }
 
     private float GetValue()

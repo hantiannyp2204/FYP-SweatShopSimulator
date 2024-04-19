0de88f81be021c7ea5415a596101f2fe9d611b01
Raydah <88923719+Raydah@users.noreply.github.com>
2024-04-19 17:59:25 +0800

    Added in Anvil minigame

--- Assets/Hammer.cs
+++ Assets/Hammer.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class Hammer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int maxHealth = 100; // Maximum health of the hammer
+    public int currentHealth; // Current health of the hammer
+    public float damagePerHit = 10f; // Damage inflicted per hit
+
+    private void Start()
     {
-        
+        currentHealth = maxHealth; // Initialize current health to maximum health
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Hit()
     {
-        
+        // Reduce the hammer's health based on damage per hit
+        currentHealth -= (int)damagePerHit;
+        Debug.Log("Hammer health:" + currentHealth);
+
+        // Check if the hammer's health has dropped to zero or below
+        if (currentHealth <= 0)
+        {
+            Destroy(gameObject); // Destroy the hammer if health is depleted
+        }
+    }
+    public void Penalty()
+    {
+        // Reduce the hammer's health based on damage per hit
+        currentHealth -= (int)damagePerHit*2;
+        Debug.Log("Hammer health:" + currentHealth);
     }
 }

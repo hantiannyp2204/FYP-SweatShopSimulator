93bc4a709bb221ac2fb085c2d9bbdb210465ef46
josh <joshng004@gmail.com>
2024-04-26 19:04:34 +0800

    mouth now closes upon collision with animation added

--- Assets/ShredderMouthCollision.cs
+++ Assets/ShredderMouthCollision.cs
@@ -1,31 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class ShredderMouthCollision : MonoBehaviour
 {
     [SerializeField] private GameObject mouth1;
     [SerializeField] private GameObject mouth2;
 
-    private bool _hasEntered;
-  
-    private void OnTriggerEnter(Collider other)
+
+    private void Start()
     {
-        _hasEntered = true;
+
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
         mouth1.GetComponent<Animator>().SetBool("isActivate", false);
-        //mouth2.GetComponent<Animator>().SetBool("isOpen", true);
+        mouth2.GetComponent<Animator>().SetBool("isActivate", false);
     }
 
     private void OnTriggerExit(Collider other)
     {
-        _hasEntered = false;
-
         mouth1.GetComponent<Animator>().SetBool("isActivate", true);
-        //mouth2.GetComponent<Animator>().SetBool("isOpen", false);
-    }
-
-    public bool GetHasEntered()
-    {
-        return _hasEntered;
+        mouth2.GetComponent<Animator>().SetBool("isActivate", true);
     }
 }

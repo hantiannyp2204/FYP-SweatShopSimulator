a3c468385638e72d781002a9c9ea855b21f1cee8
josh <joshng004@gmail.com>
2024-05-15 17:24:09 +0800

    updated climb provider

--- Assets/CustomClimbProvider.cs
+++ Assets/CustomClimbProvider.cs
@@ -6,19 +6,19 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class CustomClimbProvider : ClimbProvider
 {
     [SerializeField] Rigidbody playerRb;
-    [SerializeField] ContinuousMovementPhysics playerMovement;
+    //[SerializeField] ContinuousMovementPhysics playerMovement;
     protected override void Update()
     {
         base.Update();
         if(locomotionPhase == LocomotionPhase.Moving)
         {
             playerRb.isKinematic = true;
-            playerMovement.enabled = false;
+            //playerMovement.enabled = false;
         }
         else
         {
             playerRb.isKinematic = false;
-            playerMovement.enabled = true;
+            //playerMovement.enabled = true;
         }
     }
 }

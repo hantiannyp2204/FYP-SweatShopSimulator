558a97273e521ea6637944a07de8a9f030697c3e
TobxtR <220446q@mymail.nyp.edu.sg>
2024-05-23 02:02:16 +0800

    updated stuff on smelter
    add fully unlck and gear turning sound on smelter too

--- Assets/[Scripts]/Machines/SmelterWheel.cs
+++ Assets/[Scripts]/Machines/SmelterWheel.cs
@@ -17,14 +17,17 @@ public class SmelterWheel : XRKnob
     [Header("Feedback Events")]
     [SerializeField] private FeedbackEventData e_lock;
     [SerializeField] private FeedbackEventData e_unlock;
-    
+    [SerializeField] private FeedbackEventData e_fullyUnlocked;
+    [SerializeField] private FeedbackEventData e_wheelTurnSound;
+
     private bool fullyTurned = false;
 
     public bool GetTurnStatus()
     {
         return fullyTurned;
     }
-
+    private float currentValue = 0;
+    private bool wheelWasFullyLocked = false;
     public void CheckTurnStatus()
     {
         if(!fullyTurned && value == 1)
@@ -42,11 +45,25 @@ public class SmelterWheel : XRKnob
         }
         if(value != 0)
         {
+
+            if (Mathf.Abs(value - currentValue) >= 0.2f)
+            {
+                Debug.Log("PLAY");
+                e_wheelTurnSound?.InvokeEvent(transform.position, Quaternion.identity, transform);
+                currentValue = value;
+            }
             smelterDoor.SetAbilityToGrab(false);
+            wheelWasFullyLocked = false;
         }
         else
         {
             smelterDoor.SetAbilityToGrab(true);
+            if(!wheelWasFullyLocked)
+            {
+                e_fullyUnlocked?.InvokeEvent(transform.position, Quaternion.identity, transform);
+                wheelWasFullyLocked = true; 
+            }
+        
         }
     }
     void ResetWheel()

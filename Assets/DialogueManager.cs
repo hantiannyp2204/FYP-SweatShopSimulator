2afe5eb52e1c648bc4c989460e5d056ba4a46530
josh <joshng004@gmail.com>
2024-05-23 18:36:18 +0800

    updated scene to transition to

--- Assets/DialogueManager.cs
+++ Assets/DialogueManager.cs
@@ -51,7 +51,7 @@ public class DialogueManager : MonoBehaviour
     {
         if (_lines.Count <= 0)
         {
-            transition.GoToScene(2);
+            transition.GoToScene(0);
             EndDialogue();
             return;
         }

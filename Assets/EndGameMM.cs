21e8566d9f84fc4a531df23cc674789ea0895840
Jerald <liowjiale04@gmail.com>
2024-05-24 00:53:58 +0800

    Pushed

---
+++ Assets/EndGameMM.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGameMM : MonoBehaviour
+{
+    public GameObject _EndGameUI;
+    void Start()
+    {
+        _EndGameUI?.SetActive(false);
+    }
+
+   
+  
+}
